af4ea46b947f1df406e8d2d34671aff0a01c02b8
swagatkoirala <swagatrajkoirala@gmail.com>
2025-02-27 00:44:21 -0500

    added the base models for point evaluation

---
+++ point/models.py
@@ -0,0 +1,48 @@
+from django.db import models
+from django.conf import settings
+
+class AllocatedPoint(models.Model):
+    CHOICES= [
+        ('registration','On Registration'),
+        ('question','On Question Approve'),
+        ('answer','On Answer Approve')
+        ]
+    OPTIONS= [
+        ('active','active'),
+        ('inactive','inactive')
+        ]
+    title=models.CharField(max_length=20, choices=CHOICES, unique=True)
+    allocated_point = models.IntegerField(default=15)
+    status = models.CharField(max_length=20, choices=OPTIONS, default='active')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+    def __str__(self):
+        return str(self.title)
+
+    class Meta:
+        verbose_name_plural = "Allocated Points"
+        verbose_name = "Allocated Points"
+
+
+class Point(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='points')
+    points_title=models.ForeignKey(AllocatedPoint, on_delete=models.SET_NULL, null=True)
+    title=models.CharField(max_length=100, blank=True, null=True)
+    points_earned= models.IntegerField(default=15)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    # created_at = models.DateTimeField(null=True, blank=True)
+
+    
+
+    def __str__(self):
+        return str(self.title)
+
+    class Meta:
+        verbose_name_plural = "Points"
+        verbose_name = "Point"
+        ordering = ["-created_at",]
+
+        
