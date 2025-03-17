4bdf489ddbf25a0a55c42222ec930da1a8810e9e
Swagat Koirala <46417945+swagatkoirala@users.noreply.github.com>
2025-03-16 23:48:09 -0400

    updated the profile fields

--- authentication/models.py
+++ authentication/models.py
@@ -5,6 +5,8 @@ from django.db import models
 class CustomUser(AbstractUser):
     phone_number = models.CharField(max_length=15, blank=True, null=True)
     email = models.EmailField(unique=True, blank=False, null=False)
+    first_name = models.CharField(max_length=15, blank=True, null=True)
+    last_name = models.CharField(max_length=15, blank=True, null=True)
 
     def __str__(self):
         return self.username
