3f8d3fa8ed7dadc476c97d6c529605a6914319ab
swagatkoirala <swagatrajkoirala@gmail.com>
2025-03-18 18:21:34 -0400

    updated the profile model to add the detailed profile

--- authentication/models.py
+++ authentication/models.py
@@ -3,19 +3,23 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 class CustomUser(AbstractUser):
+    username = models.CharField(unique=True, max_length=15, blank=True, null=True)
     phone_number = models.CharField(max_length=15, blank=True, null=True)
     email = models.EmailField(unique=True, blank=False, null=False)
-    first_name = models.CharField(max_length=15, blank=True, null=True)
-    last_name = models.CharField(max_length=15, blank=True, null=True)
+    is_verified = models.BooleanField(default=False)
 
     def __str__(self):
-        return self.username
+        return self.email
     
 class Profile(models.Model):
     user = models.OneToOneField(CustomUser, on_delete=models.CASCADE, related_name='profile')
     bio = models.TextField(blank=True, null=True)
+    gender = models.CharField(max_length=15, blank=True, null=True)
+    nationality = models.CharField(max_length=15, blank=True, null=True)
+    province = models.CharField(max_length=15, blank=True, null=True)
     profile_picture = models.ImageField(upload_to='profile_pictures/', blank=True, null=True)
-    phone_number = models.CharField(max_length=15, blank=True, null=True)
+    first_name = models.CharField(max_length=15, blank=True, null=True)
+    last_name = models.CharField(max_length=15, blank=True, null=True)
 
     def __str__(self):
         return f"Profile of {self.user.username}"
