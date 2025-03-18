eaf87c82a13422cf6747089b5375f2822afdbe7b
swagatkoirala <swagatrajkoirala@gmail.com>
2025-03-18 18:22:45 -0400

    added related email verification api urls

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,7 +1,11 @@
 # authentication/urls.py
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import RegisterView, LoginView, JWTTokenView, ForgotPasswordRequestView, ResetPasswordView, ProfileViewSet
+from .views import (
+    RegisterView, LoginView, JWTTokenView, 
+    ForgotPasswordRequestView, ResetPasswordView, 
+    ProfileViewSet, VerifyEmailView
+)
 
 router = DefaultRouter()
 router.register('register', RegisterView, basename='register')
@@ -11,6 +15,7 @@ router.register('profile', ProfileViewSet, basename='profile')
 
 urlpatterns = [
     path('api/', include(router.urls)),
+    path('api/verify-email/', VerifyEmailView.as_view(), name='verify-email'),
     path('api/forgot-password/', ForgotPasswordRequestView.as_view({'post': 'create'}), name='forgot-password'),
     path('api/reset-password/', ResetPasswordView.as_view({'post': 'create'}), name='reset-password'),
 ]
