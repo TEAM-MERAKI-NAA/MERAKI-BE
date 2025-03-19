79b9e15026411d4cc8ec335db5464d4591e7c223
swagatkoirala <swagatrajkoirala@gmail.com>
2025-03-18 20:44:02 -0400

    fixed view

--- authentication/views.py
+++ authentication/views.py
@@ -42,13 +42,12 @@ class LoginView(viewsets.ViewSet):
 
     def create(self, request):
         serializer = LoginSerializer(data=request.data)
-        print(serializer, "==============")
         if serializer.is_valid():
             user = serializer.validated_data['user']
             refresh = RefreshToken.for_user(user)
             access_token = str(refresh.access_token)
             refresh_token = str(refresh)
-            return Response({'access': access_token, 'refresh': refresh_token})
+            return Response({'email': user.email,'access': access_token, 'refresh': refresh_token})
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 class JWTTokenView(viewsets.ViewSet):
