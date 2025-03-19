d4bf987c568226e60489e555f5684e978b3c1807
swagatkoirala <swagatrajkoirala@gmail.com>
2025-03-18 20:41:13 -0400

    get serializers

--- authentication/views.py
+++ authentication/views.py
@@ -20,7 +20,9 @@ import random
 from django.core.cache import cache  
 from django.conf import settings
 from rest_framework.generics import GenericAPIView
+from django.contrib.auth import get_user_model
 
+User = get_user_model()
 
 class RegisterView(viewsets.ViewSet):
     permission_classes = [AllowAny]
@@ -40,12 +42,13 @@ class LoginView(viewsets.ViewSet):
 
     def create(self, request):
         serializer = LoginSerializer(data=request.data)
+        print(serializer, "==============")
         if serializer.is_valid():
             user = serializer.validated_data['user']
             refresh = RefreshToken.for_user(user)
             access_token = str(refresh.access_token)
             refresh_token = str(refresh)
-            return Response({'user': user, 'access': access_token, 'refresh': refresh_token})
+            return Response({'access': access_token, 'refresh': refresh_token})
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 class JWTTokenView(viewsets.ViewSet):
