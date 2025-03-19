729dbad2c5cce5dacc461e30a0a635dea4484fbf
swagatkoirala <swagatrajkoirala@gmail.com>
2025-03-18 20:31:10 -0400

    fixed and completed the api for auth model

--- authentication/views.py
+++ authentication/views.py
@@ -45,7 +45,7 @@ class LoginView(viewsets.ViewSet):
             refresh = RefreshToken.for_user(user)
             access_token = str(refresh.access_token)
             refresh_token = str(refresh)
-            return Response({'access': access_token, 'refresh': refresh_token})
+            return Response({'user': user, 'access': access_token, 'refresh': refresh_token})
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 class JWTTokenView(viewsets.ViewSet):
