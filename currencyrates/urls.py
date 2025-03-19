70ab11c2b4e4922a6f8d3b3699a7ec7f3aed7edf
“AbiChitrakar” <abichitrakar2921@gmail.com>
2025-03-19 13:51:47 -0400

    Listing all the lists of CAD equivalent currency codes

--- currencyrates/urls.py
+++ currencyrates/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import all_exchange_rates, fetch_cad_exchange_rates, cad_conversion
+from .views import all_exchange_rates, fetch_cad_exchange_rates, fetch_cad_available_currencies, cad_conversion
 
 urlpatterns = [
 
     path('api/', all_exchange_rates, name='currency-rates'),
     path('api/CAD/', fetch_cad_exchange_rates, name='fetch-cad-exchange-rates'),
     path('api/exchange-rate/', cad_conversion, name='fetch-exchange-rate-by-currency'),  
+    path('api/exchange-rate/', cad_conversion, name='fetch-exchange-rate-by-currency'),  
+    path('api/cad-currencies/', fetch_cad_available_currencies, name='fetch-cad-available-currencies'),
 ]
--- currencyrates/views.py
+++ currencyrates/views.py
@@ -52,6 +52,46 @@ def fetch_cad_exchange_rates(request):
         print(f"Error fetching CAD exchange rates: {e}")
         return Response({'error': "Failed to fetch CAD data from TapTapSend API"}, status=500)
 
+
+@api_view(['GET'])
+def fetch_cad_available_currencies(request):
+    """
+    Fetches a list of all currencies available for CAD equivalency along with their country names and their respective currency.
+    """
+    try:
+        # Define the API URL and headers
+        url = "https://api.taptapsend.com/api/fxRates"
+        headers = {
+            "appian-version": "web/2022-05-03.0",
+            "x-device-id": "web",
+            "x-device-model": "web"
+        }
+
+        # Make the API request
+        response = requests.get(url, headers=headers)
+        response.raise_for_status()  # Raise an exception for HTTP errors
+        data = response.json()
+
+        # Filter for CAD exchange rates
+        cad_data = next((country for country in data['availableCountries'] if country['currency'] == 'CAD'), None)
+
+        # If CAD data is found, extract the corridors data
+        if cad_data:
+            currencies = [
+                {
+                    'Country Code': corridor.get('isoCountryCode', ''),
+                    'Country Display Name': corridor.get('countryDisplayName', ''),
+                    'Currency': corridor['currency']
+                }
+                for corridor in cad_data.get('corridors', [])
+            ]
+            return Response({'source': "Fetched from TapTapSend API", 'currencies': currencies})
+        else:
+            return Response({'error': "CAD data not found"}, status=404)
+
+    except requests.exceptions.RequestException as e:
+        print(f"Error fetching CAD exchange rates: {e}")
+        return Response({'error': "Failed to fetch CAD data from TapTapSend API"}, status=500) 
 @api_view(['GET'])
 def cad_conversion(request):
     try:
