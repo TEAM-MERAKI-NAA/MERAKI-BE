4948aa4e12d5c463a5a87593df53eabe31be224e
swagatkoirala <swagatrajkoirala@gmail.com>
2025-03-22 10:23:16 -0400

    added first name and last name for userprofile as well

--- userprofile/serializers.py
+++ userprofile/serializers.py
@@ -3,10 +3,12 @@ from .models import UserProfile
 
 class UserProfileSerializer(serializers.ModelSerializer):
     email = serializers.EmailField(source='user.email', read_only=True)
+    first_name = serializers.CharField(source='user.first_name', read_only=True)
+    last_name = serializers.CharField(source='user.last_name', read_only=True)
 
     class Meta:
         model = UserProfile
-        fields = ['id', 'email', 'bio', 
+        fields = ['id', 'email', 'first_name', 'last_name', 'bio', 
                  'profile_image', 'gender', 'nationality', 'province', 
                  'created_at', 'updated_at']
-        read_only_fields = ['created_at', 'updated_at'] 
+        read_only_fields = ['created_at', 'updated_at']
