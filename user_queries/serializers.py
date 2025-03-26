bb54cfe756bddd50bef3dd5b4147ceac7f765f5b
“AbiChitrakar” <abichitrakar2921@gmail.com>
2025-03-25 23:48:06 -0400

    Working upon pre-populating Community

--- user_queries/serializers.py
+++ user_queries/serializers.py
@@ -1,10 +1,15 @@
 from rest_framework import serializers
-from .models import Community, CommunityRequest
+from .models import Category, Community, CommunityRequest
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = ['id', 'name', 'description', 'created_at']
 
 class CommunitySerializer(serializers.ModelSerializer):
     class Meta:
         model = Community
-        fields = ['id', 'name', 'description', 'community_type', 'created_at', 'updated_at', 'image', 'members']
+        fields = ['id', 'name', 'description', 'category', 'created_at', 'updated_at', 'image', 'members']
 
 class CommunityRequestSerializer(serializers.ModelSerializer):
     class Meta:
